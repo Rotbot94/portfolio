6e9343f509b166037e5c0480b27d741f9932665f
ali <ali@dandesign.dk>
2023-08-12 14:29:51 +0200

    refactored menu item animation

--- src/app/components/menu.tsx
+++ src/app/components/menu.tsx
@@ -1,54 +1,48 @@
-import React, { useState, useRef, useEffect } from "react";
-import anime from "animejs";
+import React, { useState } from "react";
 import styles from "../styles/menu.module.css";
+import { motion } from "framer-motion";
 
 const MenuItem = (props: { title: string; url: string; itemId: number }) => {
   const { title, itemId, url } = props;
-  const [playing, setPlaying] = useState(false);
-  const [activeItemId, setActiveItemId] = useState<null | number>(null);
-  const animationRef = useRef<any>(null);
 
-  useEffect(() => {
-    if (playing && activeItemId !== null) {
-      const translateXValue = (itemId + 1) % 2 == 0 ? "-400px" : "400px";
-      const animationKeyframes = {
-        translateX: translateXValue,
-        direction: "alternate",
-        easing: "easeInOutExpo",
-      };
+  const translateXValue = itemId % 2 === 0 ? 400 : -400;
+  const [isHovered, setIsHovered] = useState(false);
 
-      animationRef.current = anime({
-        targets: `#heading${activeItemId}`,
-        ...animationKeyframes,
-        loop: true,
-        easing: "linear",
-      });
-    } else {
-      if (animationRef.current) {
-        console.log("pause");
-        animationRef.current.restart();
-        animationRef.current.pause();
-      }
-    }
-  }, [playing, activeItemId, itemId]);
+  const hoverVariants = {
+    hovered: {
+      x: translateXValue,
+      transition: {
+        ease: "linear",
+        duration: 1,
+        repeat: "infinity",
+        repeatType: "reverse",
+      },
+    },
+    notHovered: {
+      x: 0,
+      transition: {
+        duration: 0.2,
+      },
+    },
+  };
+
   return (
-    <a href={url}>
-      <li
+    <a
+      href={url}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <motion.li
+        initial={{ x: 0 }}
+        animate={isHovered ? "hovered" : "notHovered"}
+        variants={hoverVariants}
         id={"heading" + itemId}
         className={`${styles.heading} ${
-          (itemId + 1) % 2 == 0 ? "text-end" : "text-start"
+          itemId % 2 === 0 ? "text-start" : "text-end"
         } h-full cursor-pointer`}
-        onMouseEnter={() => {
-          setPlaying(true);
-          setActiveItemId(itemId);
-        }}
-        onMouseLeave={() => {
-          setPlaying(false);
-          setActiveItemId(null);
-        }}
       >
         {title}
-      </li>
+      </motion.li>
     </a>
   );
 };
